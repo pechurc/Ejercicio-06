e8d3b45c727e02573e6cba9999bd3fd1cd7d03bf
Axel Garcia <agarcia@eiva.com.ar>
2019-10-03 16:37:06 -0300

    Recuperamos los titulares codigos tambien

--- src/main/java/com/eiv/App.java
+++ src/main/java/com/eiv/App.java
@@ -1,6 +1,5 @@
 package com.eiv;
 
-
 import java.sql.Connection;
 import java.util.List;
 
@@ -10,31 +9,40 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 import com.eiv.entities.BeneficiarioEntity;
-
+import com.eiv.entities.TitularCodigo;
 
 @Configuration
 public class App {
-    
+
     public static final ApplicationContext CTX;
-    private static final String SQL = "SELECT * FROM beneficiarios";
-    @Autowired private Connection conn;
-    
+    private static final String SQL_BENEFICIARIO = "SELECT * FROM beneficiarios";
+    private static final String SQL_TITULAR_CODIGO = "SELECT * FROM titulares_codigos";
+    @Autowired
+    private Connection conn;
+
     static {
         CTX = new ClassPathXmlApplicationContext("app-config.xml");
     }
-    
+
     public static void main(String[] args) {
         App app = CTX.getBean(App.class);
         app.run();
     }
-    
+
     public void run() {
         JdbcTemplate template = new JdbcTemplate(conn);
-        
-        List<BeneficiarioEntity> beneficiarios = template.query(SQL, BeneficiarioEntity.class);
-        
+
+        List<BeneficiarioEntity> beneficiarios = template.query(SQL_BENEFICIARIO, 
+                BeneficiarioEntity.class);
+
         beneficiarios.forEach(beneficiario -> {
             System.out.println(beneficiario.toString());
+        });
+
+        List<TitularCodigo> titulares = template.query(SQL_TITULAR_CODIGO, TitularCodigo.class);
+
+        titulares.forEach(titular -> {
+            System.out.println(titular.toString());
         });
     }
 }
---
+++ src/main/java/com/eiv/entities/TitularCodigo.java
@@ -0,0 +1,54 @@
+package com.eiv.entities;
+
+import com.eiv.annotations.Mappeable;
+import com.eiv.annotations.MappedTo;
+
+@Mappeable
+public class TitularCodigo {
+    @MappedTo(columnName = "id")
+    private Long id;
+    @MappedTo(columnName = "nombre")
+    private String nombre;
+    @MappedTo(columnName = "nombre_abreviado")
+    private String nombreAbreviado;
+    @MappedTo(columnName = "direccion")
+    private String direccion;
+    
+    public Long getId() {
+        return id;
+    }
+    
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getNombre() {
+        return nombre;
+    }
+    
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    
+    public String getNombreAbreviado() {
+        return nombreAbreviado;
+    }
+    
+    public void setNombreAbreviado(String nombreAbreviado) {
+        this.nombreAbreviado = nombreAbreviado;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        this.direccion = direccion;
+    }
+
+    @Override
+    public String toString() {
+        return "TitularCodigo [id=" + id + ", nombre=" + nombre + ", nombreAbreviado=" 
+                + nombreAbreviado + ", direccion=" + direccion + "]";
+    }   
+}
